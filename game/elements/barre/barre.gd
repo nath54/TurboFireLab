9fbe872dde72c2f9344324b0ab184f22f5833da7
nathan cerisara <cerisara.nathan@gmail.com>
2021-04-09 23:28:36 +0200

    Update barre.gd

--- game/elements/barre/barre.gd
+++ game/elements/barre/barre.gd
@@ -2,16 +2,21 @@ extends StaticBody2D
 
 
 func test_collision_with_mouse():
+	# return $CollisionShape2D.shape.has_point(get_global_mouse_position())
 	return Geometry.is_point_in_polygon(get_global_mouse_position(), $Polygon2D.polygon)
 
 # partie pour le drag
 var dragging = false
 var radius = 21
 func _input(event):
-	if GlobalGame.lance_initial and event is InputEventMouseButton and event.pressed:
+	if not GlobalGame.lance_initial and event is InputEventMouseButton and event.pressed:		
 		if test_collision_with_mouse():
+			if GlobalGame.is_deleting:
+				if get_parent().get_child_count() > 1:
+					queue_free()
+				GlobalGame.is_deleting = false
 			dragging = true
 	if event is InputEventMouseButton and not event.pressed:
 		dragging = false
-	if GlobalGame.lance and dragging and event is InputEventMouseMotion:
+	if not GlobalGame.lance and dragging and event is InputEventMouseMotion:
 		position += event.relative
