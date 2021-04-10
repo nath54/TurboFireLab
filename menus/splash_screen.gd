c9fc52331c729a92f2e733ea60b81c0bb4dc835d
nathan cerisara <cerisara.nathan@gmail.com>
2021-04-10 23:44:36 +0200

    splash screen

---
+++ menus/splash_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("splash_screen")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://game/game.tscn")
